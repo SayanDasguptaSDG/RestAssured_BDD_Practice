bf55fe7b8a6ee324864b73d8140e7c5a0632694a
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2024-08-25 22:48:00 +0530

    mvn execution

---
+++ README.md
@@ -0,0 +1,9 @@
+RestAssured practice with Behavior Driver Developement (BDD) Framework
+
+To execute with maven:
+
+mvn test
+
+(using tagging)
+
+mvn test -Dcucumber.filter.tags="@tagname or @anothertagname"
--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = { "stepDefinitions" }, tags = "@DeletePlace")
+@CucumberOptions(features = "src/test/java/features", glue = { "stepDefinitions" })
 public class TestRunner {
 }
